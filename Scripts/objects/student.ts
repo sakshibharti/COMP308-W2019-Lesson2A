194ce17d225ec9050aa0ed62c4fb4b46a2551ff0
sakshi bharti <sakshibharti1@gmail.com>
2019-01-19 10:36:37 -0500

    split classes across files

---
+++ Scripts/objects/student.ts
@@ -0,0 +1,34 @@
+module objects{
+export class Student extends objects.Person{
+    //PRIVATE INSTANCE VARIABLES
+        private _studentID: string;
+
+    //PUBLIC PROPERTIES
+        get studentID(): string{
+            return this._studentID;
+
+        }
+        set studentID(newStudentID: string) {
+            this._studentID = newStudentID;
+    }
+
+
+        constructor(age:number, name:string, studentID:string) {
+            super(age, name);
+            this.studentID = studentID;
+        }
+    
+    //PRIVATE METHODS
+
+    //PUBLIC METHODS
+        public studies(): void{
+        console.log(`${this.name} with studentID: ${this.studentID} is studying`)
+    }
+}
+}
+
+
+/*
+const person: objects.Person= new objects.Person(30, "Sakshi");
+person.saysHello();
+*/
